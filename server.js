5faad60cc080b915cfdea89e8502af8c3d69958d
Mary_Varf <bma1989@yandex.ru>
2022-03-13 20:05:19 +0200

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('./db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults({
     static: './build'
 });
-const PORT = process.env.PORT || 3200; // <== You can change the port
+const PORT = process.env.PORT || 3001; // <== You can change the port
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
